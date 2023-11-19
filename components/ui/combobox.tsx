8fe1c5c78e0459cd26560a7b61ecfbc61bf32a6a
= <=>
2023-11-19 12:31:33 +0700

    completing category form

--- components/ui/combobox.tsx
+++ components/ui/combobox.tsx
@@ -31,13 +31,16 @@ export const Combobox = ({ options, value, onChange }: comboboxProps) => {
   const [open, setOpen] = React.useState(false)
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
+    <Popover
+      open={open}
+      onOpenChange={setOpen}
+    >
       <PopoverTrigger asChild>
         <Button
           variant="outline"
           role="combobox"
           aria-expanded={open}
-          className="w-[200px] justify-between"
+          className="w-full justify-between"
         >
           {value
             ? options.find(option => option.value === value)?.label
@@ -45,7 +48,7 @@ export const Combobox = ({ options, value, onChange }: comboboxProps) => {
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
+      <PopoverContent className="w-full p-0">
         <Command>
           <CommandInput placeholder="Search framework..." />
           <CommandEmpty>No option found.</CommandEmpty>
